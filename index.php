b6ec24f8da5caad243e6d7ce32b484b66c6c08a7
taako <a@a>
2020-08-16 23:03:58 +0900

    トップページに記事一覧作成

--- index.php
+++ index.php
@@ -15,44 +15,41 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main container">
-		<div class="row">
-			<div class="entry-content col-md-9">
-			<?php
-			if ( have_posts() ) :
-
-				if ( is_home() && ! is_front_page() ) :
+	<main id="primary" class="site-main">
+		<div class="container">
+			<div class="row">
+				<div class="home-area col-md-9">
+					<div class="article-area col-md-12">
+						<?php
+					if ( have_posts() ) :
+						if ( is_home() && ! is_front_page() ) :
+							?>
+							<header>
+								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+							</header>
+							<?php
+						endif;
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+							/*
+							 * Include the Post-Type-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', 'home' );
+						endwhile;
+						the_posts_navigation();
+					else :
+						// 記事が見つからなかった場合
+						get_template_part( 'template-parts/content', 'none' );
+					endif;
 					?>
-					<header>
-						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-					</header>
-					<?php
-				endif;
-
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
-
-					/*
-					 * Include the Post-Type-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_type() );
-
-				endwhile;
-
-				the_posts_navigation();
-
-			else :
-
-				get_template_part( 'template-parts/content', 'none' );
-
-			endif;
-			?>
-			</div>
-			<div class="col-md-3">
-				<?php get_sidebar(); ?>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<?php get_sidebar(); ?>
+				</div>
 			</div>
 		</div>
 	</main><!-- #main -->
