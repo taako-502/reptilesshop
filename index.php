29eeeef7ed6581e34e50a28d5af2370435f2cceb
taako <a@a>
2020-08-16 23:17:36 +0900

    見出しを表示するよう修正

--- index.php
+++ index.php
@@ -22,13 +22,12 @@ get_header();
 					<div class="article-area col-md-12">
 						<?php
 					if ( have_posts() ) :
-						if ( is_home() && ! is_front_page() ) :
+
 							?>
 							<header>
-								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+								<h1 class="page-title">記事一覧</h1>
 							</header>
 							<?php
-						endif;
 						/* Start the Loop */
 						while ( have_posts() ) :
 							the_post();
