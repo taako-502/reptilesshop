a1fd48b7442dadab4c8aba1f2e1fee461c3f63b1
taako <a@a>
2020-08-18 00:34:28 +0900

    フッターのデザイン少し変更

--- footer.php
+++ footer.php
@@ -20,10 +20,12 @@
 				?>
 			</a>
 			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'reptilesshop' ), 'reptilesshop', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+				<span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'reptilesshop' ), 'reptilesshop', '<a href="http://underscores.me/">Underscores.me</a>' );
+					?>
+				</span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
