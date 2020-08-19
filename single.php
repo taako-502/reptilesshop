ed6cd297dd5a06d98dcbce8b27c7002cc850402f
taako <a@a>
2020-08-20 00:45:40 +0900

    インデントとコメント追記

--- single.php
+++ single.php
@@ -10,43 +10,39 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-		<div class="container">
-			<div class="row">
-				<div class="main-area col-md-9">
-
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			// パンくずリスト
-			get_template_part('template-parts/content','breadcrumbs');
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'reptilesshop' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'reptilesshop' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-			// 関連記事
-			get_template_part('template-parts/content','related');
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-
-		?>
+<main id="primary" class="site-main">
+	<div class="container">
+		<div class="row">
+			<div class="main-area col-md-9">
+				<?php
+				while ( have_posts() ) :
+					the_post();
+					// パンくずリスト
+					get_template_part('template-parts/content','breadcrumbs');
+					// 本文
+					get_template_part( 'template-parts/content', get_post_type() );
+					// 次のぺーじ、前のページ
+					the_post_navigation(
+						array(
+							'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'reptilesshop' ) . '</span> <span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'reptilesshop' ) . '</span> <span class="nav-title">%title</span>',
+						)
+					);
+					// 関連記事
+					get_template_part('template-parts/content','related');
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				endwhile; // End of the loop.
+				?>
 			</div>
 			<div class="col-md-3">
 				<?php get_sidebar(); ?>
 			</div>
-			</div>
 		</div>
-	</main><!-- #main -->
+	</div>
+</main><!-- #main -->
 
 <?php
 get_sidebar();
