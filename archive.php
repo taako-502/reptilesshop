d14b876224cfad2ee66da3ab8f52aac3a43883f9
taako <a@a>
2020-08-19 20:42:03 +0900

    トップページのデザインを微修正

--- archive.php
+++ archive.php
@@ -15,34 +15,27 @@ get_header();
 			<div class="row">
 				<div class="main-area col-md-9">
 					<?php if ( have_posts() ) : ?>
-
 						<header class="page-header">
 							<?php
 							the_archive_title( '<h1 class="page-title">', '</h1>' );
 							the_archive_description( '<div class="archive-description">', '</div>' );
 							?>
 						</header><!-- .page-header -->
-
 						<?php
 						/* Start the Loop */
 						while ( have_posts() ) :
 							the_post();
-
 							/*
-							 * Include the Post-Type-specific template for the content.
-							 * If you want to override this in a child theme, then include a file
-							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							* Include the Post-Type-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
 							 */
-							get_template_part( 'template-parts/content', get_post_type() );
-
+							get_template_part( 'template-parts/content', 'home' );
+							//get_template_part( 'template-parts/content', get_post_type() );
 						endwhile;
-
 						the_posts_navigation();
-
 					else :
-
 						get_template_part( 'template-parts/content', 'none' );
-
 					endif;
 					?>
 				</div>
