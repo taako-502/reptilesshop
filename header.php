38a27b10ec0ccefb2953b12cbdfa20113d4e1bd1
taako <a@a>
2020-08-19 20:20:53 +0900

    ヘッダーのデザインを統一 固定ページにbootstrapを適用

--- header.php
+++ header.php
@@ -32,18 +32,8 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<div class="site-title-area">
-				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
+				<?php the_custom_logo(); ?>
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
-				?>
 			</div>
 			<div class="discription-area">
 				<?php
