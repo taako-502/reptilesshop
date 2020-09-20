1208b3ad1e04c1650c0137915e2edf4cc45cb681
taako-502 <i.am.armorknight@gmail.com>
2020-09-20 20:59:18 +0900

    コードフォーマット

--- assets/js/block_custom.js
+++ assets/js/block_custom.js
@@ -1,43 +1,42 @@
 /**
  * Guternbergのカスタマイズ
  */
- (function () {
-     //使用するメソッド
-     var el = wp.element.createElement,
-         blocks = wp.blocks;
+(function() {
+  //使用するメソッド
+  var el = wp.element.createElement,
+    blocks = wp.blocks;
 
-     // カスタムブロックの追加
-     blocks.registerBlockType('reptilesshop/box', {
-         title: 'ボックス',
-         icon: 'wordpress-alt',
-         category: 'layout',
-         edit: function () {
-             return el(
-                 'div',
-                 {
-                     className: "example-1",
-                     style: {
-                         backgroundColor: '#900',
-                         color: '#fff',
-                         padding: '1em',
-                     }
-                 },
-                 'エディタ側ではDIVタグ'
-             );
-         },
-         save: function () {
-             return el(
-                 'p',
-                 {
-                     className: "example-1",
-                     style: {
-                         backgroundColor: '#008000',
-                         color: '#fff',
-                         padding: '1em',
-                     }
-                 },
-                 'サイト表示側ではPタグ'
-             );
-         },
-     });
- })();
+  // カスタムブロックの追加
+  blocks.registerBlockType('reptilesshop/box', {
+    title: 'ボックス',
+    icon: 'admin-page',
+    category: 'layout',
+    edit: function() {
+      return el(
+        'div', {
+          className: "box",
+          style: {
+
+            backgroundColor: '#900',
+            color: '#fff',
+            padding: '1em',
+          }
+        },
+        'エディタ側ではDIVタグ'
+      );
+    },
+    save: function() {
+      return el(
+        'p', {
+          className: "box",
+          style: {
+            backgroundColor: '#008000',
+            color: '#fff',
+            padding: '1em',
+          }
+        },
+        'サイト表示側ではPタグ'
+      );
+    },
+  });
+})();
