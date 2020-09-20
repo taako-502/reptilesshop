7d73495ed5ba1d2406ef074dace171063dc23b72
taako-502 <i.am.armorknight@gmail.com>
2020-09-20 22:33:39 +0900

    点線ボックス追加

--- js/block_custom.js
+++ js/block_custom.js
@@ -2,6 +2,7 @@
  * Guternbergのカスタマイズ
  */
 (function() {
+  // ボックス
   var el = wp.element.createElement,
     blocks = wp.blocks,
     RichText = wp.editor.RichText;
@@ -44,6 +45,55 @@
         style: {
           background: '#f7f7f7',
           border: '2px solid',
+          padding: '0.5em 0.8em'
+        },
+        value: props.attributes.content,
+      });
+    },
+  });
+  //点線ボックス
+  var el = wp.element.createElement,
+    blocks = wp.blocks,
+    RichText = wp.editor.RichText;
+
+  blocks.registerBlockType('reptilesshop/box-dot', {
+    title: '点線ボックス',
+    icon: 'admin-page',
+    category: 'layout',
+    attributes: {
+      content: {
+        type: 'array',
+        source: 'children',
+        selector: 'div',
+      },
+    },
+    edit: function(props) {
+      var nowContent = props.attributes.content;
+      return el(
+        RichText, {
+          tagName: 'div',
+          className: "custom-box-dot",
+          style: {
+            background: '#f7f7f7',
+            border: '2px dotted',
+            padding: '0.5em 0.8em'
+          },
+          value: nowContent,
+          onChange: function(changedContent) {
+            props.setAttributes({
+              content: changedContent
+            });
+          },
+        }
+      );
+    },
+    save: function(props) {
+      return el(RichText.Content, {
+        tagName: 'div',
+        className: "custom-box-dot",
+        style: {
+          background: '#f7f7f7',
+          border: '2px dotted',
           padding: '0.5em 0.8em'
         },
         value: props.attributes.content,
